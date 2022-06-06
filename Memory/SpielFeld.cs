acccadfcd52d6d58246bd538d12feb36ad8ae758
Wapuzi <74750582+Wapuzi@users.noreply.github.com>
2022-06-06 13:31:32 +0200

    Korrektur csproject file

--- Memory/SpielFeld.cs
+++ Memory/SpielFeld.cs
@@ -9,7 +9,7 @@ namespace Memory {
         Spieler _spieler;
         int[] _feld;
 
-        
+
 
         public Spieler Spieler {
             get => _spieler;
@@ -24,44 +24,38 @@ namespace Memory {
                 _feld = value;
             }
         }
-        
+
 
         public SpielFeld(Spieler spieler, string[] feld) {
             Spieler = spieler;
             Feld = feld;
         }
 
-        public void ErzeugeSpielfeld()
-        {
+        public void ErzeugeSpielfeld() {
             Random rnd = new Random();
             int s = 4, z = 4;
-            string[] Begriffe = new string[8] {"Informatik","C#","Hello World","Array","Polymorphie","Vererbung","Visual Studio","Properties"};
-            
+            string[] Begriffe = new string[8] { "Informatik", "C#", "Hello World", "Array", "Polymorphie", "Vererbung", "Visual Studio", "Properties" };
+
             int[] anzahl = new int[8];
 
-            for (int i = 0; i < 16; i++)
-            {
+            for (int i = 0; i < 16; i++) {
                 int zufallszahl = rnd.Next(0, 8);
 
-                if (zufallszahl == i)
-                {
+                if (zufallszahl == i) {
                     anzahl[i]++;
                 }
 
-                if (anzahl[i] <= 2)
-                {
-                    for (int j = 0; i < s; i++)
-                    {
-                        for (int n = 0; n < z; n++)
-                        {
+                if (anzahl[i] <= 2) {
+                    for (int j = 0; i < s; i++) {
+                        for (int n = 0; n < z; n++) {
                             Feld[j, n] = Begriffe[zufallszahl];
 
                         }
                     }
                 }
             }
-           
+
         }
-       
+
     }
 }
