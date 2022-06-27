274d255fa1cb87f63ba3c822c0fdf77b0b41ecf7
Wapuzi <74750582+Wapuzi@users.noreply.github.com>
2022-06-27 09:14:49 +0200

    Fertig?

--- Memory/Highscore_Fenster.xaml.cs
+++ Memory/Highscore_Fenster.xaml.cs
@@ -12,163 +12,136 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace Memory
-{
+namespace Memory {
     /// <summary>
     /// Interaktionslogik für Highscore_Fenster.xaml
     /// </summary>
-    public partial class Highscore_Fenster : Window
-    {
-        public Highscore_Fenster()
-        {
+    public partial class Highscore_Fenster : Window {
+
+        public Highscore_Fenster() {
             InitializeComponent();
 
-            Computer computer = null;
+            Highscore.Datensatz[] zeile;
+            if (Highscore.ReadFromFile() == null && Highscore.ReadFromFileSchwer() == null) {
+                MessageBox.Show("Keine Highscore-Daten vorhanden!");
+            }
 
-            string zeile;
-
-            if (Computer.difficulty == "Normal")
-           {
+            if (Highscore.ReadFromFile() != null) {
 
                 zeile = Highscore.ReadFromFile();
-                string[] data = zeile.Split(';');
-                if (data[0] != null)
-                {
-                    tBox1Name.Text = data[0];
-                    tBox1Punkte.Text = data[1];
-                    tBox1Computer.Text = data[2];
+                if (zeile.Length > 1) {
+                    tBox1Name.Text = zeile[0].Name;
+                    tBox1Punkte.Text = zeile[0].Punkte.ToString();
+                    tBox1Computer.Text = zeile[0].Computer.ToString();
                 }
-                if (data.Length > 4)
-                {
+                if (zeile.Length > 2) {
 
-                    tBox2Name.Text = data[3];
-                    tBox2Punkte.Text = data[4];
-                    tBox2Computer.Text = data[5];
+                    tBox2Name.Text = zeile[1].Name;
+                    tBox2Punkte.Text = zeile[1].Punkte.ToString();
+                    tBox2Computer.Text = zeile[1].Computer.ToString();
                 }
-                if (data.Length > 7)
-                {
-                    tBox3Name.Text = data[6];
-                    tBox3Punkte.Text = data[7];
-                    tBox3Computer.Text = data[8];
+                if (zeile.Length > 3) {
+                    tBox3Name.Text = zeile[2].Name;
+                    tBox3Punkte.Text = zeile[2].Punkte.ToString();
+                    tBox3Computer.Text = zeile[2].Computer.ToString();
                 }
-                if (data.Length > 10)
-                {
-                    tBox4Name.Text = data[9];
-                    tBox4Punkte.Text = data[10];
-                    tBox4Computer.Text = data[11];
+                if (zeile.Length > 4) {
+                    tBox4Name.Text = zeile[3].Name;
+                    tBox4Punkte.Text = zeile[3].Punkte.ToString();
+                    tBox4Computer.Text = zeile[3].Computer.ToString();
                 }
-                if (data.Length > 13)
-                {
-                    tBox5Name.Text = data[12];
-                    tBox5Punkte.Text = data[13];
-                    tBox5Computer.Text = data[14];
+                if (zeile.Length > 5) {
+                    tBox5Name.Text = zeile[4].Name;
+                    tBox5Punkte.Text = zeile[4].Punkte.ToString();
+                    tBox5Computer.Text = zeile[4].Computer.ToString();
                 }
-                if (data.Length > 16)
-                {
-                    tBox6Name.Text = data[15];
-                    tBox6Punkte.Text = data[16];
-                    tBox6Computer.Text = data[17];
+                if (zeile.Length > 6) {
+                    tBox6Name.Text = zeile[5].Name;
+                    tBox6Punkte.Text = zeile[5].Punkte.ToString();
+                    tBox6Computer.Text = zeile[5].Computer.ToString();
                 }
-                if (data.Length > 19)
-                {
-                    tBox7Name.Text = data[18];
-                    tBox7Punkte.Text = data[19];
-                    tBox7Computer.Text = data[20];
+                if (zeile.Length > 7) {
+                    tBox7Name.Text = zeile[6].Name;
+                    tBox7Punkte.Text = zeile[6].Punkte.ToString();
+                    tBox7Computer.Text = zeile[6].Computer.ToString();
                 }
-                if (data.Length > 22)
-                {
-                    tBox8Name.Text = data[21];
-                    tBox8Punkte.Text = data[22];
-                    tBox8Computer.Text = data[23];
+                if (zeile.Length > 8) {
+                    tBox8Name.Text = zeile[7].Name;
+                    tBox8Punkte.Text = zeile[7].Punkte.ToString();
+                    tBox8Computer.Text = zeile[7].Computer.ToString();
                 }
-                if (data.Length > 25)
-                {
-                    tBox9Name.Text = data[24];
-                    tBox9Punkte.Text = data[25];
-                    tBox9Computer.Text = data[26];
+                if (zeile.Length > 9) {
+                    tBox9Name.Text = zeile[8].Name;
+                    tBox9Punkte.Text = zeile[8].Punkte.ToString();
+                    tBox9Computer.Text = zeile[8].Computer.ToString();
                 }
-                if (data.Length > 28)
-                {
-                    tBox10Name.Text = data[27];
-                    tBox10Punkte.Text = data[28];
-                    tBox10Computer.Text = data[29];
-                }
-            }
-            else
-            {
-                zeile = Highscore.ReadFromFileSchwer();
-                string[] data = zeile.Split(';');
-                if (data[0] != null)
-                {
-                    tBox1Names.Text = data[0];
-                    tBox1Punktes.Text = data[1];
-                    tBox1Computers.Text = data[2];
-                }
-                if (data.Length > 4)
-                {
-
-                    tBox2Names.Text = data[3];
-                    tBox2Punktes.Text = data[4];
-                    tBox2Computers.Text = data[5];
-                }
-                if (data.Length > 7)
-                {
-                    tBox3Names.Text = data[6];
-                    tBox3Punktes.Text = data[7];
-                    tBox3Computers.Text = data[8];
-                }
-                if (data.Length > 10)
-                {
-                    tBox4Names.Text = data[9];
-                    tBox4Punktes.Text = data[10];
-                    tBox4Computers.Text = data[11];
-                }
-                if (data.Length > 13)
-                {
-                    tBox5Names.Text = data[12];
-                    tBox5Punktes.Text = data[13];
-                    tBox5Computers.Text = data[14];
-                }
-                if (data.Length > 16)
-                {
-                    tBox6Names.Text = data[15];
-                    tBox6Punktes.Text = data[16];
-                    tBox6Computers.Text = data[17];
-                }
-                if (data.Length > 19)
-                {
-                    tBox7Names.Text = data[18];
-                    tBox7Punktes.Text = data[19];
-                    tBox7Computers.Text = data[20];
-                }
-                if (data.Length > 22)
-                {
-                    tBox8Names.Text = data[21];
-                    tBox8Punktes.Text = data[22];
-                    tBox8Computers.Text = data[23];
-                }
-                if (data.Length > 25)
-                {
-                    tBox9Names.Text = data[24];
-                    tBox9Punktes.Text = data[25];
-                    tBox9Computers.Text = data[26];
-                }
-                if (data.Length > 28)
-                {
-                    tBox10Names.Text = data[27];
-                    tBox10Punktes.Text = data[28];
-                    tBox10Computers.Text = data[29];
+                if (zeile.Length > 10) {
+                    tBox10Name.Text = zeile[9].Name;
+                    tBox10Punkte.Text = zeile[9].Punkte.ToString();
+                    tBox10Computer.Text = zeile[9].Computer.ToString();
                 }
             }
 
+            if (Highscore.ReadFromFileSchwer() != null) {
+                zeile = Highscore.ReadFromFile();
+                if (zeile.Length > 1) {
+                    tBox1Names.Text = zeile[0].Name;
+                    tBox1Punktes.Text = zeile[0].Punkte.ToString();
+                    tBox1Computers.Text = zeile[0].Computer.ToString();
+                }
+                if (zeile.Length > 2) {
 
-
-
-
+                    tBox2Names.Text = zeile[1].Name;
+                    tBox2Punktes.Text = zeile[1].Punkte.ToString();
+                    tBox2Computers.Text = zeile[1].Computer.ToString();
+                }
+                if (zeile.Length > 3) {
+                    tBox3Names.Text = zeile[2].Name;
+                    tBox3Punktes.Text = zeile[2].Punkte.ToString();
+                    tBox3Computers.Text = zeile[2].Computer.ToString();
+                }
+                if (zeile.Length > 4) {
+                    tBox4Names.Text = zeile[3].Name;
+                    tBox4Punktes.Text = zeile[3].Punkte.ToString();
+                    tBox4Computers.Text = zeile[3].Computer.ToString();
+                }
+                if (zeile.Length > 5) {
+                    tBox5Names.Text = zeile[4].Name;
+                    tBox5Punktes.Text = zeile[4].Punkte.ToString();
+                    tBox5Computers.Text = zeile[4].Computer.ToString();
+                }
+                if (zeile.Length > 6) {
+                    tBox6Names.Text = zeile[5].Name;
+                    tBox6Punktes.Text = zeile[5].Punkte.ToString();
+                    tBox6Computers.Text = zeile[5].Computer.ToString();
+                }
+                if (zeile.Length > 7) {
+                    tBox7Names.Text = zeile[6].Name;
+                    tBox7Punktes.Text = zeile[6].Punkte.ToString();
+                    tBox7Computers.Text = zeile[6].Computer.ToString();
+                }
+                if (zeile.Length > 8) {
+                    tBox8Names.Text = zeile[7].Name;
+                    tBox8Punktes.Text = zeile[7].Punkte.ToString();
+                    tBox8Computers.Text = zeile[7].Computer.ToString();
+                }
+                if (zeile.Length > 9) {
+                    tBox9Names.Text = zeile[8].Name;
+                    tBox9Punktes.Text = zeile[8].Punkte.ToString();
+                    tBox9Computers.Text = zeile[8].Computer.ToString();
+                }
+                if (zeile.Length > 10) {
+                    tBox10Names.Text = zeile[9].Name;
+                    tBox10Punktes.Text = zeile[9].Punkte.ToString();
+                    tBox10Computers.Text = zeile[9].Computer.ToString();
+                }
+            }
 
 
         }
 
-
+        private void Button_Click_OK(object sender, RoutedEventArgs e) {
+            DialogResult = true;
+        }
     }
 }
